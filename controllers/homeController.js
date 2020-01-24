e7328fdf044c12655f1953d6df79806c4ce27b93
FReeeZzz <freeezzzldota@gmail.com>
2020-01-24 13:50:03 +0200

    +

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -5,6 +5,7 @@ exports.index = (req, res) => {
 exports.download = (req, res) => {
     const link = "VKRNikolayCherney2.docx";
     const file = __dirname + `/../uploads/${link}`;
+    console.log(file);
     res.download(file);
 };
 
@@ -12,8 +13,9 @@ exports.upload = (req, res, next) => {
     if (req.file === null) return res.status(400).json({ msg: 'Файл не загружен' });
     let filedata = req.file;
     console.log(filedata);
+    let path = __dirname + '/../' + filedata.path;
     if(!filedata)
         res.send("Ошибка при загрузке файла");
     else
-        res.send("Файл загружен");
+        res.send("Файл загружен и находиться по адресу: " + path);
 };
